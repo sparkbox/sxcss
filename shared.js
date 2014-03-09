ad4b02418b1f94633dbc93025e28562995d170a6
ethanmuller <ethanbmuller@gmail.com>
2014-03-08 23:57:55 -0500

    asdf

--- shared.js
+++ shared.js
@@ -2,12 +2,14 @@ $(function() {
   if ($('html').hasClass('challenge')) {
     var shareText = '@sxblurthelines Done with this challenge! ' + document.URL;
     var shareHref = 'http://twitter.com/home?status=' + shareText;
-    var allDone = '<a href="' + shareHref + '" class="btn btn-primary l-fixed-br">All Done!</a>';
+    var allDone = '<a href="#" class="btn btn-primary l-fixed-br">All Done!</a>';
     var $allDone = $(allDone);
     $('body').append($allDone);
 
-    $($allDone).on('click', function(){
-      window.location.href = shareHref;
+    $($allDone).on('click', function(e){
+      e.preventDefault();
+      $allDone.off('click');
+      $(this).html('Paste into a tweet: <input type="text" value="' + shareText + '">');
     });
   }
 });
