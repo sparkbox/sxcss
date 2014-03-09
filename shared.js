4819f8510d91f0f9d4c525383fdaf202d831468d
Jeremy Mack <jeremy@pileofturtles.com>
2014-03-08 18:03:25 -0600

    feat: condiitonally add All Done

--- shared.js
+++ shared.js
@@ -1,4 +1,6 @@
 $(function() {
-  var allDone = '<a href="#" class="btn btn-primary l-fixed-br">All Done!</a>';
-  $('body').append($('<div>').html(allDone));
+  if ($('html').hasClass('challenge')) {
+    var allDone = '<a href="#" class="btn btn-primary l-fixed-br">All Done!</a>';
+    $('body').append($('<div>').html(allDone));
+  }
 });
