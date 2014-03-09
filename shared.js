16626644b262c6e093d72caf7924ada42300dee6
ethanmuller <ethanbmuller@gmail.com>
2014-03-08 23:42:44 -0500

    share link

--- shared.js
+++ shared.js
@@ -1,6 +1,8 @@
 $(function() {
   if ($('html').hasClass('challenge')) {
-    var allDone = '<a href="#" class="btn btn-primary l-fixed-br">All Done!</a>';
+    var shareText = '@sxblurthelines Done with this challenge! ' + document.URL;
+    var shareHref = 'http://twitter.com/home?status=' + shareText;
+    var allDone = '<a href="' + shareHref + '" class="btn btn-primary l-fixed-br">All Done!</a>';
     $('body').append($('<div>').html(allDone));
   }
 });
